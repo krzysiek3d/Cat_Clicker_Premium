6e8f7e09a7a06a56b0a590e8ea992272e1166b9c
Krzysztof Kacik <krzysztof.kacik@archon.pl>
2017-12-29 13:06:36 +0100

    cat clicker premium MVC

--- develope/js/main.js
+++ develope/js/main.js
@@ -1,8 +1,8 @@
-(function (param) {
+(function () {
 	'use strict';
 
 	/* ======= Model ======= */
-	var cats = {
+	var model = {
 		currentCat: null,
 		cats: [{
 				name: 'John',
@@ -33,14 +33,93 @@
 	};
 
 	/* ======= Octopus ======= */
-	var octopus = {};
+	var octopus = {
+		init: function () {
+			model.currentCat = model.cats[0];
+
+			catListView.init();
+			catDisplayView.init();
+		},
+		incrementClicks: function () {
+			model.currentCat.clicks += 1;
+		},
+		setCurrentCat: function (id) {
+			model.currentCat = model.cats[id];
+		},
+		getCurrentCat: function () {
+			return model.currentCat;
+		},
+		getCats: function () {
+			return model.cats;
+		}
+	};
 
 	/* ======= Views ======= */
-	var catListView = {};
-	var catDisplayView = {};
+	var catListView = {
+		init: function () {
+			this.catList = document.getElementsByClassName('cat-list')[0];
+			this.catList.addEventListener('click', clickHandler);
 
-	console.log('id: ', param.appId);
+			function clickHandler(event) {
+				var target = event.target,
+					id = target.value;
+				renderCurrenCat(target, id);
+			}
+
+			function renderCurrenCat(target, id) {
+				if (target.className === 'cat-list__item') {
+					octopus.setCurrentCat(id);
+					catDisplayView.render();
+				}
+			}
+			this.render();
+		},
+		render: function () {
+			var cats = octopus.getCats(),
+				cat,
+				li;
+			this.catList.innerHTML = '';
+
+			for (var i = 0, max = cats.length; i < max; i++) {
+				cat = cats[i];
+				li = document.createElement('li');
+				li.value = i;
+				li.textContent = cat.name;
+				li.classList.value = 'cat-list__item';
+				this.catList.appendChild(li);
+			}
+		}
+	};
+
+	var catDisplayView = {
+		init: function () {
+			this.catName = document.getElementsByClassName('display-cat__name')[0];
+			this.catImg = document.getElementsByClassName('display-cat__image')[0];
+			this.catClicks = document.getElementsByClassName('display-cat__click-counter')[0];
+
+			this.catImg.addEventListener('click', clickHandler);
+
+			function clickHandler(event) {
+				event.preventDefault();
+				incrementCatClicks();
+			}
+
+			function incrementCatClicks(target) {
+				octopus.incrementClicks();
+				catDisplayView.render();
+			}
+
+			this.render();
+		},
+		render: function () {
+			var currentCat = octopus.getCurrentCat();
+			this.catName.innerHTML = currentCat.name;
+			this.catImg.src = currentCat.imgSrc;
+			this.catClicks.innerHTML = currentCat.clicks;
+		}
+	};
+
+	octopus.init();
+
 	console.log('Cat-clicker version: 1.2');
-}({
-	appId: '.cat-clicker'
-}));
+}());
